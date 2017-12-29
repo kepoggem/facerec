895a5e69e9e7d874c8cc3fd4363e80646724f702
kepoggem <kevin.poggemann@t-online.de>
2017-12-29 12:05:51 +0100

    Small plt plot change

--- plot_log.py
+++ plot_log.py
@@ -19,7 +19,8 @@ args = vars(ap.parse_args())
 
 # define the paths to the training logs
 logs = [
-	(65, "training_0.log")
+	(10, "training_0.log"),
+      	(20, "training_10.log")
 ]
 
 # initialize the list of train rank-1 and rank-5 accuracies, along
