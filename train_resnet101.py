08c10ac6f807490a0520c9f7ea0a58d7ce41dccf
kepoggem <kevin.poggemann@t-online.de>
2017-12-31 00:16:05 +0100

    Change batch size for ResNet101

--- train_resnet101.py
+++ train_resnet101.py
@@ -29,7 +29,8 @@ logging.basicConfig(level=logging.DEBUG,
 	filemode="w")
 
 # determine the batch and load the mean pixel values
-batchSize = config.BATCH_SIZE * config.NUM_DEVICES
+#batchSize = config.BATCH_SIZE * config.NUM_DEVICES
+batchSize = 64
 means = json.loads(open(config.DATASET_MEAN).read())
 
 # construct the training image iterator
