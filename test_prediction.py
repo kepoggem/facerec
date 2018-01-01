dbdcaf2bd0e6581ad64d8979cd65a42a4d976d1b
kepoggem <kevin.poggemann@t-online.de>
2018-01-01 22:30:44 +0100

    Bugfix Test

--- test_prediction.py
+++ test_prediction.py
@@ -131,17 +131,20 @@ for row in rows:
 		# compute the average for each class label based on the
 		# predictions for the patches
 		#facePreds = facePreds.mean(axis=0)
+		
 		facePreds5 = np.argsort(facePreds)[::-1][:5]
 		
 		# show the true class label
 		print("[INFO] actual={}".format(target))
 		
-		print("[INFO] facePredsvalue={}".format(facePreds5))
+		print("[INFO] facePredsvalue={}".format(facePreds))
+		
+		#print("[INFO] facePredsvalue={}".format(facePreds5))
 		
 		for (i, pred) in enumerate(facePreds5):	
 			# show the predicted class label
-			print("[INFO] predicted={}".format(pred))
-			print("[INFO] ivalue={}".format(i))
+			#print("[INFO] predicted={}".format(pred))
+			#print("[INFO] ivalue={}".format(i))
 			
 			
 
