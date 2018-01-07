9e2ca77c993215d5e148f7d72470c3f1c9b7e749
kepoggem <kevin.poggemann@t-online.de>
2018-01-07 18:22:05 +0100

    Forgot output of pred label

--- live_test.py
+++ live_test.py
@@ -90,7 +90,7 @@ for row in rows:
 	for pred in enumerate(facePreds5):
 		prob = pred[1]
 		print("\t[INFO] predicted={}, probability={:.2f}%".format(
-			, facePreds5[prob] * 100))
+			pred[1], facePreds5[prob] * 100))
 
 	# show the image
 	cv2.imshow("Image", orig)
