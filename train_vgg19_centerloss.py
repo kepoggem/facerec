38f6cf952ca44a49ec4ae47eae25b7548d0d6616
kepoggem <kevin.poggemann@t-online.de>
2018-01-28 16:07:02 +0100

    Small check

--- train_vgg19_centerloss.py
+++ train_vgg19_centerloss.py
@@ -101,7 +101,8 @@ model = mx.model.FeedForward(
 # initialize the callbacks and evaluation metrics
 batchEndCBs = [mx.callback.Speedometer(batchSize, 10)]
 epochEndCBs = [mx.callback.do_checkpoint(checkpointsPath)]
-metrics = [mx.metric.Accuracy(), mx.metric.CrossEntropy(), CenterLossMetric()]
+metrics = [mx.metric.Accuracy(), mx.metric.CrossEntropy()]
+#metrics = [mx.metric.Accuracy(), mx.metric.CrossEntropy(), CenterLossMetric()]
 
 
 # train the network
