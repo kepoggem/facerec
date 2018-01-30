ad7fa1c01c660518bee1e1c8da5751ca1aeffa39
kepoggem <kevin.poggemann@t-online.de>
2018-01-30 23:20:29 +0100

    Missing tabs

--- neuralnetwork/utils/mxcenter_loss.py
+++ neuralnetwork/utils/mxcenter_loss.py
@@ -8,79 +8,79 @@ import mxnet as mx
 
 # define metric of accuracy
 class Accuracy(mx.metric.EvalMetric):
-    def __init__(self, num=None):
-        super(Accuracy, self).__init__('accuracy', num)
+	def __init__(self, num=None):
+		super(Accuracy, self).__init__('accuracy', num)
 
-    def update(self, labels, preds):
-        mx.metric.check_label_shapes(labels, preds)
+	def update(self, labels, preds):
+		mx.metric.check_label_shapes(labels, preds)
 
-        if self.num is not None:
-            assert len(labels) == self.num
-
-        pred_label = mx.nd.argmax_channel(preds[0]).asnumpy().astype('int32')
-        label = labels[0].asnumpy().astype('int32')
-
-        mx.metric.check_label_shapes(label, pred_label)
-
-        self.sum_metric += (pred_label.flat == label.flat).sum()
-        self.num_inst += len(pred_label.flat)
+		if self.num is not None:
+		    assert len(labels) == self.num
+		
+		pred_label = mx.nd.argmax_channel(preds[0]).asnumpy().astype('int32')
+		label = labels[0].asnumpy().astype('int32')
+		
+		mx.metric.check_label_shapes(label, pred_label)
+		
+		self.sum_metric += (pred_label.flat == label.flat).sum()
+		self.num_inst += len(pred_label.flat)
 
 
 # define some metric of center_loss
 class CenterLossMetric(mx.metric.EvalMetric):
-    def __init__(self):
-        super(CenterLossMetric, self).__init__('center_loss')
-
-    def update(self, labels, preds):
-        self.sum_metric += preds[1].asnumpy()[0]
-        self.num_inst += 1
+	def __init__(self):
+		super(CenterLossMetric, self).__init__('center_loss')
+	
+	def update(self, labels, preds):
+		self.sum_metric += preds[1].asnumpy()[0]
+		self.num_inst += 1
 
 
 # see details:
 # <A Discriminative Feature Learning Approach for Deep Face Recogfnition>
 class CenterLoss(mx.operator.CustomOp):
-    def __init__(self, ctx, shapes, dtypes, num_class, alpha, scale=1.0):
-        if not len(shapes[0]) == 2:
-            raise ValueError('dim for input_data shoudl be 2 for CenterLoss')
-
-        self.alpha = alpha
-        self.batch_size = shapes[0][0]
-        self.num_class = num_class
-        self.scale = scale
-
-    def forward(self, is_train, req, in_data, out_data, aux):
-        labels = in_data[1].asnumpy()
-        diff = aux[0]
-        center = aux[1]
-
-        # store x_i - c_yi
-        for i in range(self.batch_size):
-            diff[i] = in_data[0][i] - center[int(labels[i])]
-
-        loss = mx.nd.sum(mx.nd.square(diff)) / self.batch_size / 2
-        self.assign(out_data[0], req[0], loss)
-
-    def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
-        diff = aux[0]
-        center = aux[1]
-        sum_ = aux[2]
-
-        # back grad is just scale * ( x_i - c_yi)
-        grad_scale = float(self.scale/self.batch_size)
-        self.assign(in_grad[0], req[0], diff * grad_scale)
-
-        # update the center
-        labels = in_data[1].asnumpy()
-        label_occur = dict()
-        for i, label in enumerate(labels):
-            label_occur.setdefault(int(label), []).append(i)
-
-        for label, sample_index in label_occur.items():
-            sum_[:] = 0
-            for i in sample_index:
-                sum_ = sum_ + diff[i]
-            delta_c = sum_ / (1 + len(sample_index))
-            center[label] += self.alpha * delta_c
+	def __init__(self, ctx, shapes, dtypes, num_class, alpha, scale=1.0):
+		if not len(shapes[0]) == 2:
+			raise ValueError('dim for input_data shoudl be 2 for CenterLoss')
+	
+		self.alpha = alpha
+		self.batch_size = shapes[0][0]
+		self.num_class = num_class
+		self.scale = scale
+	
+	def forward(self, is_train, req, in_data, out_data, aux):
+		labels = in_data[1].asnumpy()
+		diff = aux[0]
+		center = aux[1]
+	
+		# store x_i - c_yi
+		for i in range(self.batch_size):
+			diff[i] = in_data[0][i] - center[int(labels[i])]
+	
+		loss = mx.nd.sum(mx.nd.square(diff)) / self.batch_size / 2
+		self.assign(out_data[0], req[0], loss)
+	
+	def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
+		diff = aux[0]
+		center = aux[1]
+		sum_ = aux[2]
+	
+		# back grad is just scale * ( x_i - c_yi)
+		grad_scale = float(self.scale/self.batch_size)
+		self.assign(in_grad[0], req[0], diff * grad_scale)
+	
+		# update the center
+		labels = in_data[1].asnumpy()
+		label_occur = dict()
+		for i, label in enumerate(labels):
+			label_occur.setdefault(int(label), []).append(i)
+	
+		for label, sample_index in label_occur.items():
+			sum_[:] = 0
+			for i in sample_index:
+				sum_ = sum_ + diff[i]
+			delta_c = sum_ / (1 + len(sample_index))
+			center[label] += self.alpha * delta_c
 
 
 @mx.operator.register("centerloss")
