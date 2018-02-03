52ec1a89ac772fa74faa53df122ab624b0ae61d0
kepoggem <kevin.poggemann@t-online.de>
2018-02-03 15:54:42 +0100

    Backprop batch size change

--- train_vgg19_centerloss.py
+++ train_vgg19_centerloss.py
@@ -59,7 +59,7 @@ logging.basicConfig(level=logging.DEBUG,
 
 # determine the batch and load the mean pixel values
 #batchSize = config.BATCH_SIZE * config.NUM_DEVICES
-batchSize = 32
+batchSize = 64
 means = json.loads(open(config.DATASET_MEAN).read())
 
 # construct the training image iterator
